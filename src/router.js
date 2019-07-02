9ce41ae2b8b9b645507d8b1103614844db0dc2a8
viktorChubyrka <viktor2000897@gmail.com>
2019-07-02 21:44:43 +0300

    routes - first work version

--- src/router.js
+++ src/router.js
@@ -1,10 +1,29 @@
 import Vue from "vue";
 import Router from "vue-router";
+import Clients from "./views/Clients.vue";
+import Appointments from "./views/Appointments.vue";
+import AppHomePage from "./components/AppHomePage.vue";
 
 Vue.use(Router);
 
 export default new Router({
   mode: "history",
   base: process.env.BASE_URL,
-  routes: []
+  routes: [
+    {
+      path: "/",
+      name: "Home",
+      component: AppHomePage
+    },
+    {
+      path: "/appointments",
+      name: "Appointments",
+      component: Appointments
+    },
+    {
+      path: "/clients",
+      name: "clients",
+      component: Clients
+    }
+  ]
 });
---
+++ src/views/Clients.vue
@@ -0,0 +1,69 @@
+<template>
+  <div>
+    <v-icon @click="add" v-if="isActive" large color="green">add_circle</v-icon>
+    <v-form @submit="submit" v-else method="post">
+      <v-text-field v-model="client.firstName" :counter="20" label="First Name"></v-text-field>
+      <v-text-field v-model="client.secondName" :counter="20" label="Second Name"></v-text-field>
+      <v-text-field v-model="client.phone" :counter="10" label="Phone Number"></v-text-field>
+      <v-btn
+        @click="listClients"
+        type="submit"
+        class="white--text success"
+        color="light-green accent-3"
+      >Submit</v-btn>
+      <v-btn class="white--text" color="red accent-2" @click="cancel">Cancel</v-btn>
+    </v-form>
+    <v-card>
+      <v-list>
+        <v-list-tile v-for="c in clients" :key="c.phone">
+          <v-list-tile-content>{{c.firstName}}</v-list-tile-content>
+          <v-list-tile-content>{{c.secondName}}</v-list-tile-content>
+          <v-list-tile-content>{{c.phone}}</v-list-tile-content>
+        </v-list-tile>
+      </v-list>
+    </v-card>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+const baseUrl = "http://localhost:3000/clients";
+
+export default {
+  data() {
+    return {
+      isActive: true,
+      client: {
+        firstName: "",
+        secondName: "",
+        phone: ""
+      },
+      clients: []
+    };
+  },
+  mounted() {
+    this.listClients();
+  },
+  methods: {
+    submit() {
+      axios.post(baseUrl, { body: this.client }).then(() => {});
+      this.isActive = true;
+      this.listClients();
+    },
+    add() {
+      this.isActive = false;
+    },
+    cancel() {
+      this.isActive = true;
+    },
+    listClients() {
+      axios.get(baseUrl).then(res => {
+        this.clients = [];
+        res.data.forEach(element => {
+          this.clients.push(element.body);
+        });
+      });
+    }
+  }
+};
+</script>

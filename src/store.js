7316627488c127a695f760d2c32668b25ba1f2bc
viktorChubyrka <viktor2000897@gmail.com>
2019-07-04 23:31:59 +0300

    appointment - vuex

--- src/store.js
+++ src/store.js
@@ -1,10 +1,49 @@
 import Vue from "vue";
 import Vuex from "vuex";
+import Axios from "axios";
 
 Vue.use(Vuex);
 
+const appointmentsUrl = "http://localhost:3000/appointments";
+const clientsUrl = "http://localhost:3000/clients";
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {}
+  state: {
+    appoints: [],
+    clients: []
+  },
+  getters: {
+    APPOINTS: state => {
+      return state.appoints;
+    },
+    CLIENTS: state => {
+      return state.clients;
+    }
+  },
+  mutations: {
+    SET_APPOINTS: (state, payload) => {
+      state.appoints = payload;
+    },
+    SET_CLIENTS: (state, payload) => {
+      state.clients = payload;
+    },
+    ADD_APPOINT: (state, payload) => {
+      state.appoints.push(payload);
+    }
+  },
+  actions: {
+    SAVE_APPOINT: (context, payload) => {
+      Axios.post(appointmentsUrl, payload).then(() => {
+        context.commit("ADD_APPOINT", payload);
+      });
+    },
+    GET_APPOINTS: (context, payload) => {
+      Axios.get(`${appointmentsUrl}/${payload.toString()}`).then(res => {
+        context.commit("SET_APPOINTS", res.data);
+      });
+    },
+    GET_CLIENTS: context =>
+      Axios.get(clientsUrl).then(res => {
+        context.commit("SET_CLIENTS", res.data);
+      })
+  }
 });
--- src/views/Appointments.vue
+++ src/views/Appointments.vue
@@ -7,24 +7,24 @@
             <v-menu :close-on-content-click="true" full-width min-width="290px">
               <template v-slot:activator="{ on }">
                 <v-text-field
-                  v-model="appoint.date"
+                  v-model="date"
                   label="Select data"
                   prepend-icon="event"
                   readonly
                   v-on="on"
                 ></v-text-field>
               </template>
-              <v-date-picker @change="fetchAppoints" v-model="appoint.date"></v-date-picker>
+              <v-date-picker @change="dataChanget" v-model="date"></v-date-picker>
             </v-menu>
           </v-flex>
           <v-flex xs4>
-            <v-text-field v-model="appoint.time" label="Input time"></v-text-field>
+            <v-text-field v-model="time" label="Input time"></v-text-field>
           </v-flex>
           <v-flex xs4>
             <v-form>
               <v-select
-                v-model="select"
-                :items="clients"
+                v-model="client"
+                :items="clientsList"
                 :item-text="clientInfoString"
                 label="Select"
                 persistent-hint
@@ -45,7 +45,7 @@
       </v-container>
     </v-form>
     <v-list>
-      <v-list-tile v-for="appoint in appoints" :key="appoint._id">
+      <v-list-tile v-for="appoint in appointusList" :key="appoint._id">
         <v-list-tile-content>{{appoint.time}}</v-list-tile-content>
         <v-list-tile-content>{{appoint.client.first_name}}</v-list-tile-content>
         <v-list-tile-content>{{appoint.client.last_name}}</v-list-tile-content>
@@ -56,57 +56,45 @@
 </template>
 
 <script>
-import axios from "axios";
-const clientsUrl = "http://localhost:3000/clients";
-const appointmentsUrl = "http://localhost:3000/appointments";
-
 export default {
   data() {
     return {
-      clients: [],
-      appoints: [],
-      select: {
+      client: {
         first_name: "",
         last_name: "",
         phone: "",
         _id: ""
       },
-      appoint: {
-        date: new Date().toISOString().substr(0, 10),
-        time: "",
-        client: ""
-      }
+      date: new Date().toISOString().substr(0, 10),
+      time: ""
     };
   },
-  created() {
-    this.fetchClients();
-    this.fetchAppoints();
+  computed: {
+    clientsList() {
+      return this.$store.getters.CLIENTS;
+    },
+    appointusList() {
+      return this.$store.getters.APPOINTS;
+    }
+  },
+
+  mounted() {
+    this.$store.dispatch("GET_CLIENTS");
+    this.$store.dispatch("GET_APPOINTS", this.date);
   },
 
   methods: {
     clientInfoString: client =>
       `${client.first_name} ${client.last_name} - ${client.phone}`,
+    dataChanget() {
+      this.$store.dispatch("GET_APPOINTS", this.date);
+    },
     add() {
-      this.appoint.client = this.select._id;
-      axios.post(appointmentsUrl, this.appoint).then(() => {
-        this.fetchClients();
-        this.fetchAppoints();
+      this.$store.dispatch("SAVE_APPOINT", {
+        date: this.date,
+        time: this.time,
+        client: this.client._id
       });
-    },
-    fetchClients() {
-      axios.get(clientsUrl).then(res => {
-        this.clients = [];
-        res.data.forEach(element => {
-          this.clients.push(element);
-        });
-      });
-    },
-    fetchAppoints() {
-      axios
-        .get(`${appointmentsUrl}/${this.appoint.date.toString()}`)
-        .then(res => {
-          this.appoints = res.data;
-        });
     }
   }
 };

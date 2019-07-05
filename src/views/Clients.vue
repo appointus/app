b5a007f4a911e806d7aa4fe3f19bc42c3ca4c309
Vlad <bladetheblade12@gmail.com>
2019-07-05 16:06:24 +0300

    aded client edit

--- src/views/Clients.vue
+++ src/views/Clients.vue
@@ -15,10 +15,23 @@
     </v-form>
     <v-card>
       <v-list>
+        <v-form v-if="isEdit">
+          <v-text-field :counter="20" label="New Name" v-model="client.first_name"></v-text-field>
+          <v-text-field :counter="20" label="New Second Name" v-model="client.last_name"></v-text-field>
+          <v-text-field :counter="10" label="New Phone Number" v-model="client.phone"></v-text-field>
+          <v-btn
+            @click="save"
+            type="button"
+            class="white--text success"
+            color="light-green accent-3"
+          >Save</v-btn>
+          <v-btn class="white--text" color="red accent-2" @click="cancel">Cancel</v-btn>
+        </v-form>
         <v-list-tile v-for="client in clients" :key="client.phone">
           <v-list-tile-content>{{client.first_name}}</v-list-tile-content>
           <v-list-tile-content>{{client.last_name}}</v-list-tile-content>
           <v-list-tile-content>{{client.phone}}</v-list-tile-content>
+          <v-btn color="info" @click="edit(client)">Edit</v-btn>
         </v-list-tile>
       </v-list>
     </v-card>
@@ -34,7 +47,8 @@ export default {
         first_name: "",
         last_name: "",
         phone: ""
-      }
+      },
+      isEdit: false
     };
   },
   computed: {
@@ -53,9 +67,20 @@ export default {
     },
     add() {
       this.isFormHidden = false;
+      this.client = {};
     },
     cancel() {
       this.isFormHidden = true;
+      this.isEdit = false;
+    },
+    edit(client) {
+      this.isEdit = true;
+      this.client = client;
+    },
+    save() {
+      this.$store.dispatch("updateClient", this.client);
+
+      this.isEdit = false;
     }
   }
 };
